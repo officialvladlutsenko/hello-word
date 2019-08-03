b4e910b59a30c348396cfeff0bd0578ac53114fa
officialvladlutsenko <53622155+officialvladlutsenko@users.noreply.github.com>
2019-08-03 12:08:52 +0300

    Добавил прогрумму

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num_1; 
+            Console.WriteLine("Enter first num:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter second num:");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result is:" + (num + num_1).ToString ());
+            Console.ReadKey ();
+        }
+    }
+}
